252a30a7da2a02c20e77540c663c788919fcf918
Malinka0-0 <129773831+Malinka0-0@users.noreply.github.com>
2023-06-04 18:49:12 +0300

    Add files via upload

---
+++ Study.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CubHelper
+{
+    public partial class Study : Form
+    {
+        public Study()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(1, 55); // Генеруємо випадкове число від 1 до 54
+
+            string imagePath = $"D:\\Project\\Programing\\cub\\oll\\{randomNumber}.png"; // Складаємо шлях до випадкового зображення
+
+            pictureBox1.Image = Image.FromFile(imagePath); // Встановлюємо зображення в PictureBox
+
+
+        }
+    }
+}
