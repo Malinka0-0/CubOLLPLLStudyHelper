9b85ef13108bc06c6dab6b44dea883319744b13d
Malinka0-0 <129773831+Malinka0-0@users.noreply.github.com>
2023-06-11 22:13:18 +0300

    Update Study.cs

--- Study.cs
+++ Study.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace CubHelper
@@ -16,16 +17,17 @@ namespace CubHelper
 
         public Study()
         {
+            Formulas formulas = new Formulas();
             InitializeComponent();
-            string[] options = { "Option 1", "Option 2", "Option 3" };
-            comboBox1.Items.AddRange(options);
+           
+            comboBox1.Items.AddRange(formulas.Spliter("name"));
         }
 
         private void Study_Load(object sender, EventArgs e)
         {
             Formulas formulas = new Formulas();
             pictureBox1.Image = Image.FromFile($"D:\\Project\\Programing\\cub\\oll\\1.png"); // Встановлюємо зображення в PictureBox
-            label1.Text = formulas.OLL[0];
+            label1.Text = formulas.Spliter("form",0);
         }
        
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
@@ -33,7 +35,7 @@ namespace CubHelper
             Formulas formulas = new Formulas();
 
             pictureBox1.Image = Image.FromFile($"D:\\Project\\Programing\\cub\\oll\\{comboBox1.SelectedIndex + 1}.png"); // Встановлюємо зображення в PictureBox
-            label1.Text = formulas.OLL[comboBox1.SelectedIndex];
+            label1.Text = formulas.Spliter("form", comboBox1.SelectedIndex);
         }
 
 
